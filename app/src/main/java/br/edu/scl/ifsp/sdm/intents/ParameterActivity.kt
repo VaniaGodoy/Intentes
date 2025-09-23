03680129bb931e1ba9ded882166263a6528496a3
Vania Godoy <vaniafatima@msn.com>
2025-09-23 15:45:05 -0300

    primeiro commit

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
@@ -1,7 +1,9 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityParameterBinding
 
 class ParameterActivity : AppCompatActivity() {
@@ -13,9 +15,18 @@ class ParameterActivity : AppCompatActivity() {
         setContentView(activityParameterBinding.root)
         setSupportActionBar(activityParameterBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
+        intent.getStringExtra(PARAMETER_EXTRA)?.let {
+            activityParameterBinding.parameterEt.setText(it)
+        }
 
-        activityParameterBinding.returnCloseBt.setOnClickListener{
-
+        activityParameterBinding.apply {
+            returnCloseBt.setOnClickListener{
+                val resultIntent = Intent().apply {
+                    putExtra(PARAMETER_EXTRA, parameterEt.text.toString())
+                }
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
         }
     }
 }
